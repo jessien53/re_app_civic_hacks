415c99c52c78609f5f54fed025f8c7e13ac07aeb
jessien53 <jnoun2@uic.edu>
2025-02-22 18:22:33 -0500

    Changing Re.mp4 location to new assets folder

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'questionnaire_screen.dart';
-
+// need to implement animation for opening app
+// path to mp4: app/src/main/res/Re.mp4
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
